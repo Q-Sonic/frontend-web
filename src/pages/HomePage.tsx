aa72d76529e48c6abd476dd890af04d19ac04511
Dylan España Cervantes <gownerbeats@gmail.com>
2026-03-04 21:44:45 -0500

    prueba

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ export function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold underline">
-        Home
+        Home - dahsboard
       </h1>
       <Link to="/auth">Auth</Link>
     </div>
